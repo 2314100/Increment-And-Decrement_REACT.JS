0d5660129478719fb751ee5a8b694a8affd62ab4
Fardeen <itzfardeen143@gmail.com>
2024-04-08 21:20:35 +0530

    first commit

---
+++ ReactFolio/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import IncrementAndDecrement from './IncrementAndDecrement'
+
+function App() {
+
+  return (
+    <>
+      <IncrementAndDecrement />
+    </>
+  )
+}
+
+export default App;
